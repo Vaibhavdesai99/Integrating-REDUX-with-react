fc60b79136c6acaaec9d9bebc7145fc7971f1745
Vaibhavdesai99 <vaibhavdesai510@gmail.com>
2023-06-07 00:06:32 +0530

    Integrating Redux with react

---
+++ src/components/Store/index.js
@@ -0,0 +1,23 @@
+// Here we need to first import createStore
+import { createStore } from "redux";
+
+// This is reducer function who change the state of central store
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    // Increment the counter by 2 when "increment" action is dispatched
+    return {
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
+  return state;
+};
+
+// Create the store by passing the counterReducer function
+const store = createStore(counterReducer);
+
+export default store;
